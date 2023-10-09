c5f9fd1aa1b5846775b68624d479441735681c5e
khunguraharman <khungura_harman@hotmail.com>
2023-10-09 15:34:17 -0700

    Changed  Model movement and rotation to New Input System Actions

--- Assets/Scripts/MoveModel.cs
+++ Assets/Scripts/MoveModel.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 using System;
+using UnityEngine.InputSystem;
 
 public class MoveModel : MonoBehaviour
 {
     private Vector2 XZBounds = LeftHandPresence.XZ_Boundary_Offset;
     // Start is called before the first frame update
-    private InputDevice LH = LeftHandPresence.hand_controller;
-    private InputDevice RH = RightHandPresence.hand_controller;
+    private UnityEngine.XR.InputDevice LH = LeftHandPresence.hand_controller;
+    private UnityEngine.XR.InputDevice RH = RightHandPresence.hand_controller;
     private Quaternion Default_Rotation;
 
     private float[] scalors = new float[] { 0.5f, 0.01f, 0.005f, 0.001f, 0.0001f };
 
     private Vector2 X_bounds;
     private Vector2 Z_bounds;
+
+    private InputActionAsset default_actions_asset;
+    
+    private InputAction move;
+    private InputAction rotate;
+    private InputAction pressedA;
 
     private void Awake()
     {
@@ -27,62 +34,68 @@ public class MoveModel : MonoBehaviour
         Z_bounds[1] = LeftHandPresence.spawn_point.z - XZBounds.y;
         Debug.Log("X bounds are: " + X_bounds);
         Debug.Log("Y bounds are: " + Z_bounds);
-    }   
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        ControlTranslation();
-        ControlRotations();
-    }
-
-    void ControlTranslation()
-    {
-        Vector2 command = new Vector2(0,0);
-        if (LH.TryGetFeatureValue(CommonUsages.primary2DAxis, out command)) //if both 0, do nothing
-        {
-            int i = 0;
-            float move_LR = scalors[i] * command[0];
-            while ((gameObject.transform.position.x + move_LR >= X_bounds[0] || gameObject.transform.position.x + move_LR <= X_bounds[1]) && i < scalors.Length-1) 
-            {
-                i++;
-                move_LR = scalors[i] * command[0];
-            }
-
-            i = 0;
-            float move_UD = scalors[i] * command[1];
-            while ((gameObject.transform.position.z + move_UD >= Z_bounds[0] || gameObject.transform.position.z + move_UD <= Z_bounds[1]) && i < scalors.Length-1)
-            {
-                i++;
-                move_LR = scalors[i] * command[1];
-            }
-            LeftHandPresence.CurrentLesson.UpdateModelBounds();
-            /*
-            float move_LR = 0.5f*command[0];
-            if(gameObject.transform.position.x + move_LR >= LeftHandPresence.spawn_point.x + XZBounds.x || gameObject.transform.position.x + move_LR <= LeftHandPresence.spawn_point.x  - XZBounds.x)
-            {
-                move_LR = 0;
-            }
-
-            float move_UD = 0.5f * command[1];
-            if (gameObject.transform.position.z + move_UD >= LeftHandPresence.spawn_point.z + XZBounds.y || gameObject.transform.position.z + move_UD <=   LeftHandPresence.spawn_point.z - XZBounds.y)
-            {
-                move_UD = 0;
-            }            
-            */
-            Vector3 translation = new Vector3(move_LR, 0, move_UD);
-            gameObject.transform.Translate(0.02f * translation, Space.World);
-            //new Cognitive3D.CustomEvent("Translated Model").SetProperties(new Dictionary<string, object> { { "Model Position", "Model Translation" } }).Send(gameObject.transform.position);
+        default_actions_asset = Resources.Load<InputActionAsset>("XRI Default Input Actions");
+        //Ensure both are
+        move = default_actions_asset.FindActionMap("XRI LeftHand Locomotion").FindAction("Move");
+        rotate = default_actions_asset.FindActionMap("XRI RightHand Locomotion").FindAction("Move");
+        pressedA = default_actions_asset.FindActionMap("XRI RightHand Interaction").FindAction("PressedA");
+        if (move==null) 
+        { 
+            Debug.Log("could not find move"); 
         }
+        else
+        {
+            Debug.Log("found move");
+        }
+        if (rotate == null)
+        {
+            Debug.Log("could not find rotate");
+        }
+        else
+        {
+            Debug.Log("found rotate");
+        }
+        move.performed += ControlTranslation;
+        move.Enable();
+        rotate.performed += ControlRotations;
+        rotate.Enable();
+        pressedA.performed += ResetOrientation;
+        pressedA.Enable(); 
+
+    }      
+
+    void ControlTranslation(InputAction.CallbackContext context)
+    {        
+        Vector2 command = context.ReadValue<Vector2>();
+        int i = 0;
+        float move_LR = scalors[i] * command[0];
+        while ((gameObject.transform.position.x + move_LR >= X_bounds[0] || gameObject.transform.position.x + move_LR <= X_bounds[1]) && i < scalors.Length-1) 
+        {
+            i++;
+            move_LR = scalors[i] * command[0];
+        }
+
+        i = 0;
+        float move_UD = scalors[i] * command[1];
+        while ((gameObject.transform.position.z + move_UD >= Z_bounds[0] || gameObject.transform.position.z + move_UD <= Z_bounds[1]) && i < scalors.Length-1)
+        {
+            i++;
+            move_LR = scalors[i] * command[1];
+        }
+        LeftHandPresence.CurrentLesson.UpdateModelBounds();
+            
+        Vector3 translation = new Vector3(move_LR, 0, move_UD);
+        gameObject.transform.Translate(0.02f * translation, Space.World);
+        //new Cognitive3D.CustomEvent("Translated Model").SetProperties(new Dictionary<string, object> { { "Model Position", "Model Translation" } }).Send(gameObject.transform.position);
+       
     }
 
-    void ControlRotations()
-    {
-        Vector2 command = new Vector2(0, 0);
-        if (RH.TryGetFeatureValue(CommonUsages.primary2DAxis, out command)) //if both 0, do nothing
-        {
-            //Vector3 spin = new Vector3(command[0], command[1], 0);
-            if(Math.Abs(command[0]) >= Math.Abs(command[1]))
+    void ControlRotations(InputAction.CallbackContext context)
+    {        
+        Vector2 command = context.ReadValue<Vector2>();
+        
+        if (Math.Abs(command[0]) >= Math.Abs(command[1]))
             {
                 gameObject.transform.RotateAround(gameObject.transform.position, Vector3.up, 0.5f * command[0]);
             }
@@ -94,14 +107,21 @@ public class MoveModel : MonoBehaviour
 
             //new Cognitive3D.CustomEvent("Rotated Model").SetProperties(new Dictionary<string, object> { { "Model Orientation", "Model Rotation" } }).Send(gameObject.transform.eulerAngles);
 
-            LeftHandPresence.CurrentLesson.UpdateModelBounds();
-        }
-        bool PressedA;
-        if(RH.TryGetFeatureValue(CommonUsages.primaryButton, out PressedA) && PressedA)
-        {
-            gameObject.transform.rotation = Default_Rotation;
+            LeftHandPresence.CurrentLesson.UpdateModelBounds();               
+    }
+    private void OnDisable()
+    {
+        move.performed -= ControlTranslation;
+        move.Disable();
+        rotate.performed -= ControlRotations;
+        rotate.Disable();
+        pressedA.performed -= ResetOrientation;
+        pressedA.Disable();
+    }
+    void ResetOrientation(InputAction.CallbackContext context)
+    {
+        gameObject.transform.rotation = Default_Rotation;
 
-            LeftHandPresence.CurrentLesson.UpdateModelBounds();
-        }
+        LeftHandPresence.CurrentLesson.UpdateModelBounds();
     }
 }
